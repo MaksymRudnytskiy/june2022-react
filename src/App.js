c30fdcbfc3873a9df3d03e1396a81a4b2ac2d7d8
MaksymRudnytskiy <therudmax@gmail.com>
2022-10-04 17:37:31 +0300

    lesson 6 +

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import {useReducer, useRef} from "react";
+
+
+const reducer = (state, action) => {
+    const {type, payload} = action
+
+    switch (type) {
+        case 'addCat':
+            return {...state, cats: [...state.cats, {name: payload, id: Date.now()}]}
+        case 'deleteCat':
+            return {...state, cats: state.cats.filter(cat => cat.id !== payload)}
+        case 'addDog':
+            return {...state, dogs: [...state.dogs, {name: payload, id: Date.now()}]}
+        case 'deleteDog':
+            return {...state, dogs: state.dogs.filter(dog => dog.id !== payload)}
+        default:
+            return state
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, dispatch] = useReducer(reducer, {cats: [], dogs: []})
+
+    const catInput = useRef()
+    const dogInput = useRef()
+
+    const addCat = () => {
+        dispatch({type: 'addCat', payload: catInput.current.value})
+        catInput.current.value = ''
+    }
+
+    const addDog = () => {
+        dispatch({type: 'addDog', payload: dogInput.current.value})
+        dogInput.current.value = ''
+    }
+
+    return (
+        <div className={'father'}>
+            <div className={'wrap'}>
+                <div className={'top'}>
+                    <div>
+                        <label>Cats Name: <input type={'text'} ref={catInput}/></label>
+                        <button onClick={addCat}>Add Cat</button>
+                    </div>
+                </div>
+                {
+                    state.cats.map(cat => (<ul key={cat.id}>
+                        <li>{cat.name}
+                            <button onClick={() => dispatch({type: 'deleteCat', payload: cat.id})}>Delete Cat</button>
+                        </li>
+                    </ul>))
+                }
+            </div>
+            <div className={'wrap'}>
+                <div className={'top'}>
+                    <div>
+                        <label>Dog Name: <input type={'text'} ref={dogInput}/></label>
+                        <button onClick={addDog}>Add Dog</button>
+                    </div>
+                </div>
+                {
+                    state.dogs.map(dog => (<ul key={dog.id}>
+                        <li>{dog.name}
+                            <button onClick={() => dispatch({type: 'deleteDog', payload: dog.id})}>Delete Dog</button>
+                        </li>
+                    </ul>))
+                }
+            </div>
+
+
+        </div>
+    );
 }
 
 export default App;
